e236b321d660fda0d5d526cf98b6473719b94d21
mutaible0 <ketan@mutaible.com>
2025-02-26 12:24:41 -0500

    initial commit

--- grid2op/Backend/__init__.py
+++ grid2op/Backend/__init__.py
@@ -2,3 +2,4 @@ __all__ = ["Backend", "PandaPowerBackend"]
 
 from grid2op.Backend.backend import Backend
 from grid2op.Backend.pandaPowerBackend import PandaPowerBackend
+from grid2op.Backend.pandaModelsBackend import PandaModelsBackend
